869eb36b060a61ad927bcd7d9b05186364d14daf
fqianlei <fqianlei888@163.com>
2020-05-23 06:46:30 +0800

    第二次更新，实现了菜单目录

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'security.apps.SecurityConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
